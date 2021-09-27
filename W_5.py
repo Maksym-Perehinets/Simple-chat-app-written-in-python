39ef8f2144838b0ec31710c15ed3a1b8b20ef863
Maksym-Perehinets <90769932+Maksym-Perehinets@users.noreply.github.com>
2021-09-27 18:57:50 +0300

    Домашня робота за 5 відео урок

---
+++ W_5.py
@@ -0,0 +1,39 @@
+z =77**6
+print('if you want add num enter 1',
+             'substarction enter 2 ',
+             'multiplying enter 3',
+             'division enter 4 ', sep ='\n')
+
+def add(a, b ):
+    print(a+b)
+
+def sub(a, b):
+    print(a-b)
+                        #Функції математичних дій
+def mult(a, b):
+    print(a*b)
+
+def div(a, b):
+    if b > 0:
+        print(a/b)
+    else:
+        print('eror')
+
+add(4,5)
+while z == 77**6:
+    oper = int(input('enter hire '))
+    num1 = float(input('enter your number'))
+    num2 = float(input('enter your number'))
+    if oper == 1:
+        add(num1,num2)
+    elif oper == 2:
+        sub(num1, num2)
+    elif oper == 3:
+       mult(num1,num2)
+    elif oper == 4:
+        div(num1,num2 )
+    z = input('To exit enter any namber') or  77**6
+else:
+    print('thx for using code')
+
+
